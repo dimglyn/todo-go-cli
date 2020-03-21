c1b5fe045922b76a279d0fa9c2b13e6e81faa5d0
Dimtiris <dim.glynatsis@gmail.com>
2020-03-21 22:28:29 +0200

    add remove flow in main program

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,35 +14,44 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Tell me what to do: ")
 	for scanner.Scan() {
-	
+
 		text := scanner.Text()
-		
+
 		if text == "quit" {
 			break
 		}
+
 		if text == "show" {
 			RenderTodos(repo)
 		}
-		
+
 		args, command := parseInput(text)
 
-		todo := Todo {
-			text: args, 
-			id: i,
+		todo := Todo{
+			text: args,
+			id:   i,
 		}
-		
+
 		if command == "add" {
 			repo, i = AppendTodo(repo, todo)
 			fmt.Println("Success added todo with id: ", todo.id)
 		}
-		
-		
+		if command == "remove" {
+			if removeIndex, err := strconv.ParseInt(args, 10, 32); err == nil {
+				if err, repo = RemoveTodo(repo, int(removeIndex)); err == nil {
+					fmt.Println("Removed todo with id: ", removeIndex)
+				}
+			} else {
+				fmt.Println(err)
+			}
+		}
+
 		fmt.Print("Tell me what to do: ")
 	}
 	fmt.Println(repo)
 }
 
-func parseInput (text string) (args string, command string) {
+func parseInput(text string) (args string, command string) {
 	inputArray := strings.Split(text, " ")
 	command = inputArray[0]
 	args = strings.Join(inputArray[1:], " ")
