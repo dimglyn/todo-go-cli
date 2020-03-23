0fa152a699a26ae49b524218ee1ffca73ad1ca3f
Dimtiris <dim.glynatsis@gmail.com>
2020-03-23 23:26:22 +0200

    refactor: move executeQuery in handler.go

--- handler.go
+++ handler.go
@@ -2,7 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+func executeQuery(repo TodoRepo, query Query) TodoRepo {
+	switch query.command {
+	case "show", "view":
+		fmt.Println(repo)
+		break
+	case "add", "new", "create":
+		repo, _ = newTodo(repo, query.args)
+		break
+	case "edit", "update":
+		query := parseInput(query.args)
+		if id, err := strconv.ParseInt(query.command, 10, 32); err == nil {
+			repo, _ = updateTodo(repo, int(id), query.args)
+			break
+		}
+	case "toggle", "done":
+		if index, err := strconv.ParseInt(query.args, 10, 32); err == nil {
+			repo = toggleDone(repo, int(index))
+			break
+		}
+	case "remove", "delete", "rm":
+		if removeIndex, err := strconv.ParseInt(query.args, 10, 32); err == nil {
+			repo = deleteTodo(repo, int(removeIndex))
+			break
+		}
+	default:
+		fmt.Println("Sorry cant do that")
+	}
+
+	return repo
+}
 
 func newTodo(repo TodoRepo, args string) (TodoRepo, int) {
 	i := len(repo)
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -27,40 +26,8 @@ func main() {
 		}
 		query := parseInput(text)
 		query.command = strings.ToLower(query.command)
-		todoRepo = execute(todoRepo, query)
+		todoRepo = executeQuery(todoRepo, query)
 		fmt.Print("Tell me what to do: ")
 	}
 	fmt.Println(todoRepo)
-}
-
-func execute(repo TodoRepo, query Query) TodoRepo {
-
-	switch query.command {
-	case "show", "view":
-		fmt.Println(repo)
-		break
-	case "add", "new", "create":
-		repo, _ = newTodo(repo, query.args)
-		break
-	case "edit", "update":
-		query := parseInput(query.args)
-		if id, err := strconv.ParseInt(query.command, 10, 32); err == nil {
-			repo, _ = updateTodo(repo, int(id), query.args)
-			break
-		}
-	case "toggle", "done":
-		if index, err := strconv.ParseInt(query.args, 10, 32); err == nil {
-			repo = toggleDone(repo, int(index))
-			break
-		}
-	case "remove", "delete", "rm":
-		if removeIndex, err := strconv.ParseInt(query.args, 10, 32); err == nil {
-			repo = deleteTodo(repo, int(removeIndex))
-			break
-		}
-	default:
-		fmt.Println("Sorry cant do that")
-	}
-
-	return repo
 }
