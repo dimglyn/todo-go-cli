528965ed7aaae599afdfc2d894d497f5bfa337c1
Dimtiris <dim.glynatsis@gmail.com>
2020-03-25 22:01:41 +0200

    use comand enums
    Co-authored-by: Elias Vlahos <elvlachos@gmail.com>

--- query.go
+++ query.go
@@ -20,43 +20,29 @@ type Query struct {
 	args    QueryArgs
 }
 
-var validCommands = map[string]Command{
-	"show":   1,
-	"view":   1,
-	"add":    2,
-	"new":    2,
-	"create": 2,
-	"edit":   3,
-	"update": 3,
-	"toggle": 4,
-	"done":   4,
-	"remove": 5,
-	"delete": 5,
-	"rm":     5,
-}
-
 const (
 	show Command = iota + 1
 	add
 	edit
 	toggle
 	remove
+	unknown = -1
 )
 
 func parseInput(query string) (Query, error) {
 	valid, _ := validQuery(query)
 	if !valid {
-		return Query{command: -1}, errors.New(fmt.Sprint("not a valid query"))
+		return Query{command: unknown}, errors.New(fmt.Sprint("not a valid query"))
 	}
 
 	tokens := strings.Split(query, " ")
 
 	command := getCommand(tokens[0])
 
-	if contains([]int{3, 4, 5}, int(command)) {
+	if contains([]Command{edit, toggle, remove}, command) {
 		id, err := strconv.ParseInt(tokens[1], 10, 32)
 		if err != nil {
-			return Query{command: -1}, errors.New(fmt.Sprint("where is todo id?"))
+			return Query{command: unknown}, errors.New(fmt.Sprint("where is todo id?"))
 		}
 		return Query{
 			command: command,
@@ -65,7 +51,7 @@ func parseInput(query string) (Query, error) {
 				text:   strings.Join(tokens[2:], " "),
 			},
 		}, nil
-	} else if command == 2 {
+	} else if command == add {
 		args := QueryArgs{text: strings.Join(tokens[1:], " ")}
 		return Query{command, args}, nil
 	}
@@ -73,11 +59,26 @@ func parseInput(query string) (Query, error) {
 }
 
 func getCommand(command string) Command {
+	validCommands := map[string]Command{
+		"show":   1,
+		"view":   1,
+		"add":    2,
+		"new":    2,
+		"create": 2,
+		"edit":   3,
+		"update": 3,
+		"toggle": 4,
+		"done":   4,
+		"remove": 5,
+		"delete": 5,
+		"rm":     5,
+	}
+
 	command = strings.ToLower(command)
 	if com, ok := validCommands[command]; ok {
 		return com
 	}
-	return -1
+	return unknown
 }
 
 func validQuery(q string) (bool, error) {
@@ -85,7 +86,7 @@ func validQuery(q string) (bool, error) {
 	return regexp.MatchString(queryRegex, q)
 }
 
-func contains(arr []int, n int) bool {
+func contains(arr []Command, n Command) bool {
 	for _, val := range arr {
 		if val == n {
 			return true
