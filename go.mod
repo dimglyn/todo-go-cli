9b4cbad95be5d2204887a78673938608433cd7dd
Dimtiris <dim.glynatsis@gmail.com>
2020-03-28 01:28:25 +0200

    add discordgo and parse token

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dimGlyn/todo-go-cli
 
 go 1.14
 
-require go.mongodb.org/mongo-driver v1.3.1
+require (
+	github.com/bwmarrin/discordgo v0.20.2 // indirect
+	go.mongodb.org/mongo-driver v1.3.1
+)
--- main.go
+++ main.go
@@ -2,36 +2,41 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"os"
-	"strings"
 )
 
-var todoRepo TodoRepo
-var scanner *bufio.Scanner
+var (
+	Token    string
+	todoRepo TodoRepo
+	scanner  *bufio.Scanner
+)
 
 func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
 	todoRepo = TodoRepo{}
-	scanner = bufio.NewScanner(os.Stdin)
-	fmt.Print("Tell me what to do: ")
 }
 
 func main() {
-	for scanner.Scan() {
 
-		text := strings.TrimSpace(scanner.Text())
-		if text == "quit" || text == "exit" {
-			fmt.Println("Bye bye")
-			break
-		}
-		query, err := parseInput(text)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Print("Tell me what to do: ")
-			continue
-		}
-		todoRepo = handle(todoRepo, query)
-		fmt.Print("Tell me what to do: ")
-	}
-	fmt.Println(todoRepo)
+	fmt.Println(Token)
+
+	// for scanner.Scan() {
+
+	// 	text := strings.TrimSpace(scanner.Text())
+	// 	if text == "quit" || text == "exit" {
+	// 		fmt.Println("Bye bye")
+	// 		break
+	// 	}
+	// 	query, err := parseInput(text)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		fmt.Print("Tell me what to do: ")
+	// 		continue
+	// 	}
+	// 	todoRepo = handle(todoRepo, query)
+	// 	fmt.Print("Tell me what to do: ")
+	// }
+	// fmt.Println(todoRepo)
 }
